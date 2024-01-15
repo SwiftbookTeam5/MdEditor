5a82553f503e6770020ff6a148cab983639681d4
Aleksandra Riazanova <darlingsandra@yandex.ru>
2024-01-16 00:13:31 +0700

    feat: add unit test for TodoList scene

---
+++ Project/Tests/Scenes/TodoList/TodoListInteractorTests.swift
@@ -0,0 +1,85 @@
+//
+//  TodoListInteractorTests.swift
+//  MdEditor
+//
+//  Created by Александра Рязанова on 15.01.2024.
+//  Copyright © 2024 SwiftbookTeam5. All rights reserved.
+//
+
+import XCTest
+import TaskManagerPackage
+@testable import MdEditor
+
+final class TodoListInteractorTests: XCTestCase {
+
+	// MARK: - Properties
+
+	private let presenter = TodoListPresenterSpy()
+	private let sectionManager = SectionForTaskManagerAdapterMock()
+	
+	// MARK: - Methods
+
+	func test_fetchData_shouldBeSuccess() {
+		let sut = makeSut()
+		
+		sut.fetchData()
+		
+		XCTAssertTrue(presenter.isCalledPresent, "Не вызван presenter.present(:)")
+	}
+	
+	func test_createTask_shouldBeSuccess() {
+		let sut = makeSut()
+		
+		sut.createTask()
+		
+		XCTAssertTrue(presenter.isCalledCreateTask, "Не вызван presenter.createTask()")
+	}
+}
+
+// MARK: - Private methods
+
+private extension TodoListInteractorTests {
+
+	func makeSut() -> TodoListInteractor {
+		TodoListInteractor(presenter: presenter, sectionManager: sectionManager)
+	}
+}
+
+final class SectionForTaskManagerAdapterMock: ISectionForTaskManagerAdapter {
+
+	// MARK: - Properties
+
+	private let currentSection: Section = .uncompleted
+
+	// MARK: - Methods
+
+	func getSections() -> [Section] {
+		[currentSection]
+	}
+
+	func getSection(forIndex index: Int) -> Section {
+		currentSection
+	}
+
+	func getTasksForSection(section: Section) -> [TaskManagerPackage.Task] {
+		[]
+	}
+}
+
+final class TodoListPresenterSpy: ITodoListPresenter {
+
+	// MARK: - Properties
+
+	private(set) var isCalledPresent = false
+	private(set) var isCalledCreateTask = false
+
+	// MARK: - Methods
+
+	func present(response: TodoListModel.Response) {
+		isCalledPresent = true
+	}
+
+	func createTask() {
+		isCalledCreateTask = true
+	}
+}
---
+++ Project/Tests/Scenes/TodoList/TodoListPresenterTests.swift
@@ -0,0 +1,47 @@
+//
+//  TodoListPresenterTests.swift
+//  MdEditorTests
+//
+//  Created by Александра Рязанова on 15.01.2024.
+//  Copyright © 2024 SwiftbookTeam5. All rights reserved.
+//
+
+import XCTest
+import TaskManagerPackage
+@testable import MdEditor
+
+final class TodoListPresenterTests: XCTestCase {
+
+	// MARK: - Properties
+
+	private let viewController = TodoListViewControllerSpy()
+
+	// MARK: - Methods
+
+	func test_present_withValidRequest_shouldBeSuccess() {
+		let sut = makeSut()
+		let response = TodoListModel.Response(data: [])
+		
+		sut.present(response: response)
+		
+		XCTAssertTrue(viewController.isCalledRender, "Не вызван viewController.render(:)")
+	}
+}
+
+// MARK: - Private methods
+
+private extension TodoListPresenterTests {
+
+	func makeSut() -> TodoListPresenter {
+		TodoListPresenter(viewController: viewController, createTaskClosure: nil)
+	}
+}
+
+final class TodoListViewControllerSpy: ITodoListViewController {
+
+	private(set) var isCalledRender = false
+
+	func render(viewModel: MdEditor.TodoListModel.ViewModel) {
+		isCalledRender = true
+	}
+}
