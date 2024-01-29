5d4b5f4a4a98b637956ebf8cea9451ac81e834ab
Dmitriy <snipere@list.ru>
2024-01-30 00:05:57 +1000

    add localization for ui tests, alert is localized

--- Project/Sources/Coordinators/LoginCoordinator.swift
+++ Project/Sources/Coordinators/LoginCoordinator.swift
@@ -49,8 +49,8 @@ final class LoginCoordinator: ILoginCoordinator {
 	}
 
 	func showError(message: String) {
-		let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-		let action = UIAlertAction(title: "Ок", style: .default)
+		let alert = UIAlertController(title: L10n.AlertAuth.title, message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: L10n.AlertAuth.okActionTitle, style: .default)
 		alert.addAction(action)
 		navigationController.present(alert, animated: true, completion: nil)
 	}
