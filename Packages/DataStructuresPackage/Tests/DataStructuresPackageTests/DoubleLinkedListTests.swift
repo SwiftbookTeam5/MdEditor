abc92bab9c0f80c09058172f1354008f0b64eec4
Dmitriy <snipere@list.ru>
2024-02-05 19:07:48 +1000

    fix: edit DoubleLinkedListTests

--- Packages/DataStructuresPackage/Tests/DataStructuresPackageTests/DoubleLinkedListTests.swift
+++ Packages/DataStructuresPackage/Tests/DataStructuresPackageTests/DoubleLinkedListTests.swift
@@ -10,433 +10,144 @@ import XCTest
 
 final class DoubleLinkedListTests: XCTestCase {
 
-	func test_initList_withValue_shouldHaveCorrectCount() {
-		let sut = DoubleLinkedList(value: 5)
-
-		XCTAssertEqual(
-			sut.count,
-			1,
-			"Неверное количество элементов в списке при инициализации"
-		)
-	}
-
-	func test_initList_withoutValue_shouldHaveCorrectCount() {
+	func test_init_emptyList_shouldBeEmpty() {
 		let sut = DoubleLinkedList<Int>()
 
-		XCTAssertEqual(
-			sut.count,
-			0,
-			"Неверное количество элементов в списке при инициализации со значением"
-		)
+		XCTAssertTrue(sut.isEmpty, "Список должен быть пуст.")
+		XCTAssertEqual(sut.count, 0, "Количество элементов с списке должно быть равно 0.")
+		XCTAssertNil(sut.headValue, "Начало списка должно быть пустым.")
+		XCTAssertNil(sut.tailValue, "Конец списка должен быть пуст.")
 	}
 
-	func test_initList_withValue_shouldHaveCorrectValue() {
-		let value = 1
+	func test_init_withValue_shouldBeCorrect() {
+		let value = 5
+
 		let sut = DoubleLinkedList(value: value)
 
-		XCTAssertEqual(
-			sut.value(at: 0),
-			value,
-			"Неверное значение элемента в списке при инициализации"
-		)
+		XCTAssertFalse(sut.isEmpty, "Список не должен быть пуст.")
+		XCTAssertEqual(sut.count, 1, "Количество элементов в списке должно быть равно 1.")
+		XCTAssertEqual(sut.headValue, value, "Начало списка содержит неверное значение.")
+		XCTAssertEqual(sut.tailValue, value, "Конец списка содержит неверное значение.")
 	}
 
-	func test_push_withValue_shouldHaveCorrectCount() {
-
-		// arrange
+	func test_push_twoValue_shouldBeCorrect() {
 		var sut = DoubleLinkedList<Int>()
 
-		// act
-		sut.push(5)
+		sut.push(1)
+		sut.push(2)
 
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			1,
-			"Неверное количество элементов в списке при добавлении в начало списка"
-		)
+		XCTAssertFalse(sut.isEmpty, "Список не должен быть пуст.")
+		XCTAssertEqual(sut.count, 2, "Количество элементов в списке должно быть равно 2.")
+		XCTAssertEqual(sut.headValue, 2, "Начало списка содержит неверное значение.")
+		XCTAssertEqual(sut.tailValue, 1, "Конец списка содержит неверное значение.")
 	}
 
-	func test_push_withValue_shouldHaveCorrectValue() {
-
-		// arrange
-		let value = 1
+	func test_append_twoValue_shouldBeCorrect() {
 		var sut = DoubleLinkedList<Int>()
 
-		// act
-		sut.push(value)
+		sut.append(1)
+		sut.append(2)
 
-		// assert
-		XCTAssertEqual(
-			sut.value(at: 0),
-			value,
-			"Неверное значение элемента в списке при добавлении в начало списка"
-		)
+		XCTAssertFalse(sut.isEmpty, "Список не должен быть пуст.")
+		XCTAssertEqual(sut.count, 2, "Количество элементов в списке должно быть равно 2.")
+		XCTAssertEqual(sut.headValue, 1, "Начало списка содержит неверное значение.")
+		XCTAssertEqual(sut.tailValue, 2, "Конец списка содержит неверное значение.")
 	}
 
-	func test_append_withValue_shouldHaveCorrectCount() {
+	func test_insert_threeValuesInListWithTwoElements_shouldBeCorrect() {
+		let numbers = [1, 3]
+		var sut = makeSUT(with: numbers)
 
-		// arrange
-		var sut = DoubleLinkedList<Int>()
+		sut.insert(2, after: 0)
+		sut.insert(4, after: 2)
+		sut.insert(6, after: 4)
 
-		// act
-		sut.append(5)
-
-		// assert
+		XCTAssertFalse(sut.isEmpty, "Список не должен быть пуст.")
+		XCTAssertEqual(sut.headValue, 1, "Начало списка содержит неверное значение.")
+		XCTAssertEqual(sut.tailValue, 4, "Конец списка содержит неверное значение.")
 		XCTAssertEqual(
 			sut.count,
-			1,
-			"Неверное количество элементов в списке при добавлении в конец списка"
+			4,
+			"Количество элементов должно быть равно 4, т.к. один эелемент добавлялся после несуществующего индекса"
 		)
 	}
 
-	func test_append_withValue_shouldHaveCorrectValue() {
-
-		// arrange
-		let value = 5
-		var sut = DoubleLinkedList<Int>()
-
-		// act
-		sut.append(value)
-
-		// assert
-		XCTAssertEqual(
-			sut.value(at: 0),
-			value,
-			"Неверное значение элемента в списке при добавлении в конец списка"
-		)
-	}
-
-	func test_insert_withValue_shouldHaveCorrectCount() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.insert(10, after: 0)
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count + 1,
-			"Неверное количество элементов в списке при вставке в список"
-		)
-	}
-
-	func test_insert_withValue_shouldHaveCorrectValue() {
-
-		// arrange
-		let value = 10
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.append(value)
-
-		// assert
-		XCTAssertEqual(
-			sut.value(at: numbers.count),
-			value,
-			"Неверное значение элемента при вставке в список"
-		)
-	}
-
-	func test_insert_withEmptyList_shouldBeFail() {
-		var sut = DoubleLinkedList<Int>()
-
-		sut.insert(5, after: 0)
-
-		XCTAssertEqual(
-			sut.count,
-			0,
-			"Неверное количество элементов после вставки в список по существующему индексу"
-		)
-	}
-
-	func test_insert_withNotExistingAfterIndex_shouldBeFail() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.insert(10, after: numbers.count + 1)
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count,
-			"Неверное количество элементов после вставки в список по существующему индексу"
-		)
-	}
-
-	func test_pop_withFullList_shouldHaveCorrectCount() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.pop()
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count - 1,
-			"Неверное количество элементов после извлечения из начала списка"
-		)
-	}
-
-	func test_pop_withFullList_shouldHaveCorrectValue() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		let value = sut.pop()
-
-		// assert
-		XCTAssertTrue(
-			numbers.first == value,
-			"Неверное значение извлеченного элемента из начала списка"
-		)
-	}
-
-	func test_pop_withEmptyList_shouldBeNilValue() {
-		var sut = DoubleLinkedList<Int>()
-
-		let value = sut.pop()
-
-		XCTAssertNil(
-			value,
-			"Ошибка, при удалении из пустого списка вернулось значение"
-		)
-	}
-
-	func test_pop_withOneNode_shouldBeEmpty() {
-
-		// arrange
-		var sut = DoubleLinkedList<Int>()
-		sut.push(5)
-
-		// act
-		sut.pop()
-
-		// assert
-		XCTAssertTrue(
-			sut.isEmpty,
-			"Ошибка, при удалении единственного элемента список не пустой"
-		)
-	}
-
-	func test_removeLast_withFullList_shouldHaveCorrectCount() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.removeLast()
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count - 1,
-			"Неверное количество элементов после извлечения из конца списка"
-		)
-	}
-
-	func test_removeLast_withFullList_shouldHaveCorrectValue() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		let value = sut.removeLast()
-
-		// assert
-		XCTAssertTrue(
-			numbers.last == value,
-			"Неверное значение извлеченного элемента из конца списка"
-		)
-	}
-
-	func test_removeLast_withEmptyList_shouldBeNilValue() {
-		var sut = DoubleLinkedList<Int>()
-
-		let value = sut.removeLast()
-
-		XCTAssertNil(
-			value,
-			"Неверное значение извлеченного элемента с конца пустого списка"
-		)
-	}
-
-	func test_removeLast_withOneNode_shouldBeEmpty() {
-
-		// arrange
-		var sut = DoubleLinkedList<Int>()
-		sut.push(5)
-
-		// act
-		sut.removeLast()
-
-		// assert
-		XCTAssertTrue(
-			sut.isEmpty,
-			"Ошибка, при извлечении с конца списка последнего единственного элемента список не пустой"
-		)
-	}
-
-	func test_remove_withIndex_shouldHaveCorrectCount() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.remove(after: 0)
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count - 1,
-			"Неверное количество элементов в списке при удалении по индексу"
-		)
-	}
-
-	func test_remove_withIndex_shouldHaveCorrectValue() {
-
-		// arrange
-		let index = 1
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		let value = sut.remove(after: index - 1)
-
-		// assert
-		XCTAssertTrue(
-			numbers[index] == value,
-			"Неверное значение удаляемого элемента из списка"
-		)
-	}
-
-	func test_remove_withNotExistingAfterIndex_shouldBeFail() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.remove(after: numbers.count + 1)
-
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count,
-			"Не корректное количество эелментов после удаления из списка по несуществующему индексу"
-		)
-	}
-
-	func test_remove_withFullList_shouldHaveCorrectCount() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		var sut = makeSUT(with: numbers)
-
-		// act
-		sut.remove(after: 0)
-		
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count - 1,
-			"Не корректное количество элементов после удаления из списка"
-		)
-	}
-
-	func test_remove_withSmallList_shouldHaveCorrectCount() {
-
-		// arrange
+	func test_pop_threeValuesFromListWithTwoElements_shouldBeCorrect() {
 		let numbers = [1, 2]
 		var sut = makeSUT(with: numbers)
 
-		// act
-		sut.remove(after: 0)
+		let removedFirstValue = sut.pop()
+		let removedSecondValue = sut.pop()
+		let removedThirdValue = sut.pop()
 
-		// assert
-		XCTAssertEqual(
-			sut.count,
-			numbers.count - 1,
-			"Не корректное количество элементов после удаления из списка"
-		)
-	}
-	
-	func test_value_withOneNode_shouldHaveCorrectValue() {
-
-		// arrange
-		let firstValue = 2
-		var sut = DoubleLinkedList<Int>()
-		sut.push(firstValue)
-
-		// act
-		let currentValue = sut.value(at: 0)
-
-		// assert
-		XCTAssertTrue(
-			firstValue == currentValue,
-			"Не корректное значение получаемого элемента"
-		)
+		XCTAssertTrue(sut.isEmpty, "Список должен быть пуст.")
+		XCTAssertEqual(sut.count, 0, "Количество элементов в списке должно быть равно 0.")
+		XCTAssertEqual(removedFirstValue, 1, "Список вернул неверное значение.")
+		XCTAssertEqual(removedSecondValue, 2, "Список вернул неверное значение.")
+		XCTAssertNil(removedThirdValue, "Список вернул несуществующее значение.")
+		XCTAssertNil(sut.headValue, "Начало списка должно быть пустым.")
+		XCTAssertNil(sut.tailValue, "Конец списка должен быть пуст.")
 	}
 
-	func test_value_withLastIndex_shouldHaveCorrectValue() {
+	func test_removeLast_threeValuesFromListWithTwoElements_shouldBeCorrect() {
+		let numbers = [1, 2]
+		var sut = makeSUT(with: numbers)
 
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+		let removedFirstValue = sut.removeLast()
+		let removedSecondValue = sut.removeLast()
+		let removedThirdValue = sut.removeLast()
+		
+		XCTAssertTrue(sut.isEmpty, "Список должен быть пуст.")
+		XCTAssertEqual(sut.count, 0, "Количество элементов в списке должно быть равно 0.")
+		XCTAssertEqual(removedFirstValue, 2, "Список вернул неверное значение.")
+		XCTAssertEqual(removedSecondValue, 1, "Список вернул неверное значение.")
+		XCTAssertNil(removedThirdValue, "Список вернул несуществующее значение.")
+		XCTAssertNil(sut.headValue, "Начало списка должно быть пустым.")
+		XCTAssertNil(sut.tailValue, "Конец списка должен быть пуст.")
+	}
+
+	func test_removeAfter_zeroIndexThreeValuesFromListWithThreeElements_shouldBeCorrect() {
+		let numbers = [1, 2, 3]
+		var sut = makeSUT(with: numbers)
+
+		let removedFirstValue = sut.remove(after: 0)
+		let removedSecondValue = sut.remove(after: 0)
+		let removedThirdValue = sut.remove(after: 0)
+
+		XCTAssertFalse(sut.isEmpty, "Список не должен быть пуст.")
+		XCTAssertEqual(sut.count, 1, "Количество элементов в списке должно быть равно 1.")
+		XCTAssertEqual(removedFirstValue, 2, "Список вернул неверное значение.")
+		XCTAssertEqual(removedSecondValue, 3, "Список вернул неверное значение.")
+		XCTAssertNil(removedThirdValue, "Список вернул несуществующее значение.")
+		XCTAssertEqual(sut.headValue, 1, "Начало списка содержит неверное значение.")
+		XCTAssertEqual(sut.tailValue, 1, "Конец списка содержит неверное значение.")
+	}
+
+	func test_valueAtIndex_shouldBeCorrectValues() {
+		let numbers = [1, 2, 3, 4, 5]
 		let sut = makeSUT(with: numbers)
 
-		// act
-		let currentValue = sut.value(at: 7)
+		let valueAt0 = sut.value(at: 0)
+		let valueAt1 = sut.value(at: 1)
+		let valueAt2 = sut.value(at: 2)
+		let valueAt3 = sut.value(at: 3)
+		let valueAt4 = sut.value(at: 4)
+		let valueAt5 = sut.value(at: 5)
 
-		// assert
-		XCTAssertEqual(
-			currentValue,
-			8,
-			"Не корректное значение получаемого элемента"
-		)
+		XCTAssertEqual(valueAt0, 1, "Список вернул неверное значение.")
+		XCTAssertEqual(valueAt1, 2, "Список вернул неверное значение.")
+		XCTAssertEqual(valueAt2, 3, "Список вернул неверное значение.")
+		XCTAssertEqual(valueAt3, 4, "Список вернул неверное значение.")
+		XCTAssertEqual(valueAt4, 5, "Список вернул неверное значение.")
+		XCTAssertNil(valueAt5, "Список вернул несуществующее значение.")
 	}
 
-	func test_value_withFirstIndex_shouldHaveCorrectValue() {
-
-		// arrange
-		let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-		let sut = makeSUT(with: numbers)
-
-		// act
-		let currentValue = sut.value(at: 2)
-
-		// assert
-		XCTAssertEqual(
-			currentValue,
-			3,
-			"Не корректное значение получаемого элемента"
-		)
-	}
-	
 	func test_description_withValues_shouldHaveCorrectValue() {
-
-		// arrange
 		let numbers = [1, 2, 3]
 		let sut = makeSUT(with: numbers)
 
-		// act
 		let description = sut.description
 
-		// assert
 		XCTAssertEqual(
 			description,
 			"1 <--> 2 <--> 3",
